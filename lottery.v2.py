285228de2247fa361ead46590ef9ed4b65c25b4f
lybing <bing.dev@outlook.com>
2018-08-30 19:55:20 +0800

    新增分析每个数字在开球顺序位上被击中的频率

---
+++ lottery.v2.py
@@ -0,0 +1,107 @@
+﻿import requests
+import re
+import pymysql
+from pyecharts import Bar, Pie, Bar3D
+from lotterymodel import PeriodModel,WinningNumberModel,SalesInfoModel
+
+def getContent():
+    url = 'https://datachart.500.com/ssq/history/newinc/outball.php?start=01001&end=18100'
+    return requests.get(url).text
+
+def parsePage():
+    try:
+        htmlContent = getContent()
+        # htmlContent = htmlContent[htmlContent.index('tbody'):]
+
+        # 这里要注意Python中正则表达式的分组要加P，如(?P<groupName>5555)
+        reg_rows = re.finditer('\<td[^>]*?>(\d{5})\<\/td\>\<td[^>]*?>(\d{4}-\d{2}-\d{2})\<\/td\><td[^>]+>(?P<num1>\d{2})\<\/td\>\<td[^>]+>(?P<num2>\d{2})\<\/td\>\<td[^>]+>(?P<num3>\d{2})\<\/td\>\<td[^>]+>(?P<num4>\d{2})\<\/td\>\<td[^>]+>(?P<num5>\d{2})\<\/td\>\<td[^>]+>(?P<num6>\d{2})\<\/td\>.*?font4\"\>(?P<blue>\d+)', htmlContent)
+        for tr_item in reg_rows:
+            period = PeriodModel()
+            period.openDate = tr_item.group(2)
+            period.openPeriod = tr_item.group(1)
+
+            winningNumber = WinningNumberModel()
+            winningNumber.RedNumber1 = tr_item.group('num1')
+            winningNumber.RedNumber2 = tr_item.group('num2')
+            winningNumber.RedNumber3 = tr_item.group('num3')
+            winningNumber.RedNumber4 = tr_item.group('num4')
+            winningNumber.RedNumber5 = tr_item.group('num5')
+            winningNumber.RedNumber6 = tr_item.group('num6')
+            winningNumber.BlueNumber = tr_item.group('blue')
+            
+            storeLotteryInfo(period, winningNumber)
+
+    except Exception as err:
+        print(err)
+    finally:
+        print('ok')
+  
+def storeLotteryInfo(period, winningNumber):
+    try:
+        conn = getConnection()
+        with conn.cursor() as cursor:
+            # Create a new record
+            sqlPeriod = "INSERT INTO `period_v2` (`OpenDate`, `OpenPeriod`) VALUES (%s, %s)"
+            cursor.execute(sqlPeriod, (period.openDate, period.openPeriod))
+            
+            sqlWinningNumber = "INSERT INTO `winningnumbers_v2` (`periodId`, `RedNumber1`, `RedNumber2`, `RedNumber3`, `RedNumber4`, `RedNumber5`, `RedNumber6`, `BlueNumber`) SELECT LAST_INSERT_ID(), %s, %s, %s, %s, %s, %s, %s"
+            cursor.execute(sqlWinningNumber, (winningNumber.RedNumber1, winningNumber.RedNumber2, winningNumber.RedNumber3, winningNumber.RedNumber4, winningNumber.RedNumber5, winningNumber.RedNumber6, winningNumber.BlueNumber))
+        # connection is not autocommit by default. So you must commit to save
+        # your changes.
+        conn.commit()
+    except Exception as storeError:
+        print(storeError)
+    finally:
+        conn.close()
+
+def getConnection():
+    return pymysql.connect(host='localhost',
+                             user='root',
+                             password='1a2B3///',
+                             db='lottery',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+
+def getFrequency(data_row, colIdx):
+    return (data_row['num%dCount'%colIdx]/(data_row['num1Count']+data_row['num2Count']+data_row['num3Count']+data_row['num4Count']+data_row['num5Count']+data_row['num6Count']+data_row['num7Count']))*100
+
+def analysisFrequency_Pie3D():
+    # 使用cursor()方法获取操作游标 
+    conn  = getConnection()
+    cursor = conn.cursor()
+    
+    try:
+        # 执行SQL语句
+        cursor.execute('CALL Proc_FrequencyOnEachPosition_v2()')
+        # 获取所有记录列表
+        results = cursor.fetchall()
+        
+        ds = []
+        x_axis_list = [i for i in range(1, 34)]
+        y_axis_list = [i for i in range(1, 8)]
+
+        for num in x_axis_list:
+            for num_position in y_axis_list:
+                frequency = getFrequency(results[num-1],num_position)
+                ds.append([num, num_position, frequency])
+
+        bar3D = Bar3D()
+        bar3D.use_theme('dark')
+        #realisticMaterial={"roughness": 0.3, "metalness": 1},
+        bar3D.add("每个位置上出现的频率", x_axis_list, y_axis_list, ds, environment="#000", grid3d_shading='realistic',
+        postEffect={"enable": "true","SSAO":{"enable": "true","radius": 5}}, 
+        light={ "main":{"intensity":3}, "ambientCubemap":{"texture": 'data-gl/asset/pisa.hdr',"exposure": 1,"diffuseIntensity": 0.5,"specularIntensity": 2} },
+        itemStyle={ "color": '#ccc'}, zAxis3D={"type":'value',"max":10, "min":0 },
+        barSize=4,bevelSize=0.4,bevelSmoothness=4, emphasis={"label":{"show":"false"}})
+        
+        bar3D.render()
+    except Exception as queryError:
+        print(queryError)
+    finally:
+        conn.close()
+
+# 解析开奖信息Html
+# parsePage()
+
+# 以Pie的方式展示每个数字的频率
+analysisFrequency_Pie3D()
--- lotteryProc.sql
+++ lotteryProc.sql
@@ -34,4 +34,46 @@ BEGIN
 	DROP TEMPORARY TABLE IF EXISTS temp_number5; 
 	DROP TEMPORARY TABLE IF EXISTS temp_number6; 
 	DROP TEMPORARY TABLE IF EXISTS temp_number7;
+END;
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `Proc_FrequencyOnEachPosition_v2`()
+BEGIN
+	#Routine body goes here...
+	CREATE TEMPORARY TABLE temp_number_list 
+	select 1 num union select 2 union select 3 union select 4 union select 5 union select 6 union select 7 union select 8 union select 9 
+	union select 10 union select 11 union select 12 union select 13 union select 14 union select 15 union select 16 union select 17 union select 18 
+	union select 19 union select 20 union select 21 union select 22 union select 23 union select 24 union select 25 union select 26 
+	union select 27 union select 28 union select 29 union select 30 union select 31 union select 32 union select 33; 
+	
+  CREATE TEMPORARY TABLE temp_number1 SELECT RedNumber1 RedNumber, count(RedNumber1) as numCount from winningnumbers_v2 group by RedNumber1; 
+	CREATE TEMPORARY TABLE temp_number2 SELECT RedNumber2 RedNumber, count(RedNumber2) as numCount from winningnumbers_v2 group by RedNumber2; 
+	CREATE TEMPORARY TABLE temp_number3 SELECT RedNumber3 RedNumber, count(RedNumber3) as numCount from winningnumbers_v2 group by RedNumber3; 
+	CREATE TEMPORARY TABLE temp_number4 SELECT RedNumber4 RedNumber, count(RedNumber4) as numCount from winningnumbers_v2 group by RedNumber4; 
+	CREATE TEMPORARY TABLE temp_number5 SELECT RedNumber5 RedNumber, count(RedNumber5) as numCount from winningnumbers_v2 group by RedNumber5; 
+	CREATE TEMPORARY TABLE temp_number6 SELECT RedNumber6 RedNumber, count(RedNumber6) as numCount from winningnumbers_v2 group by RedNumber6; 
+	CREATE TEMPORARY TABLE temp_blueNumber SELECT BlueNumber, count(BlueNumber) as numCount from winningnumbers_v2 group by BlueNumber; 
+	
+	SELECT nList.num,
+				IFNULL(n1.numCount,0) num1Count, IFNULL(n2.numCount,0) num2Count
+				,IFNULL(n3.numCount,0) num3Count, IFNULL(n4.numCount,0) num4Count
+				,IFNULL(n5.numCount,0) num5Count, IFNULL(n6.numCount,0) num6Count
+				,IFNULL(n7.numCount,0) num7Count 
+	from temp_number_list nList 
+	left join temp_number1 n1 on n1.RedNumber=nList.num 
+	left join temp_number2 n2 on n2.RedNumber=nList.num 
+	left join temp_number3 n3 on n3.RedNumber=nList.num 
+	left join temp_number4 n4 on n4.RedNumber=nList.num 
+	left join temp_number5 n5 on n5.RedNumber=nList.num 
+	left join temp_number6 n6 on n6.RedNumber=nList.num 
+	left join temp_blueNumber n7 on n7.BlueNumber=nList.num; 
+	
+	DROP TEMPORARY TABLE IF EXISTS temp_number1; 
+	DROP TEMPORARY TABLE IF EXISTS temp_number2; 
+	DROP TEMPORARY TABLE IF EXISTS temp_number3; 
+	DROP TEMPORARY TABLE IF EXISTS temp_number4; 
+	DROP TEMPORARY TABLE IF EXISTS temp_number5; 
+	DROP TEMPORARY TABLE IF EXISTS temp_number6; 
+	DROP TEMPORARY TABLE IF EXISTS temp_blueNumber;
+
+	DROP TEMPORARY TABLE IF EXISTS temp_number_list; 
 END
